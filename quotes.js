0972896eef2578d5a1554241103a11499611a509
In-dustrialist <pinaevmsk@gmail.com>
2025-09-16 12:11:05 +0400

    Adding Modules

---
+++ quotes.js
@@ -0,0 +1,20 @@
+const quotes = [
+  {
+    quote: "The best way to get started is to quit talking and begin doing.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "Don't let yesterday take up too much of today.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "You learn more from failure than from success.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "It's not whether you get knocked down, it's whether you get up.",
+    author: "Steve Jobs",
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-// Массив с цитатами
-const quotes = [
-  {
-    quote: "The best way to get started is to quit talking and begin doing.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "Don't let yesterday take up too much of today.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "You learn more from failure than from success.",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "It's not whether you get knocked down, it's whether you get up.",
-    author: "Steve Jobs",
-  },
-];
+import quotes from "./quotes.js";
 
-// Находим элементы
 const generateBtn = document.getElementById("generateBtn"); // кнопка
 const quoteElement = document.getElementById("quote"); // место для цитаты
 
